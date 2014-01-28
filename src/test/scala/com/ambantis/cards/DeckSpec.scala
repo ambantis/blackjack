55c8247786110dcbb79b1a6c65bb7999e45b801b
Alexandros Bantis <ambantis@gmail.com>
2014-01-28 12:05:28 -0800

    passes test of cards value

--- src/test/scala/com/ambantis/cards/DeckSpec.scala
+++ src/test/scala/com/ambantis/cards/DeckSpec.scala
@@ -14,16 +14,34 @@ class DeckSpec extends FunSpec with Matchers with BeforeAndAfter {
     cards4 = Deck(4).shuffle
   }
 
-  describe("A single deck of playing cards") {
-    it("should have 52 cards") {
-      cards should have length (52)
-    }
-  }
-
   describe("four decks of playing cards mixed together") {
     it("should have 52*4 cards") {
       cards4 should have length (4*52)
     }
   }
+
+  describe("A single deck of playing cards") {
+    it("should have 52 cards") {
+      cards should have length (52)
+    }
+
+    it("should have an equal number of cards in each suit") {
+      val cardsInEachSuit: List[Int] = cards.groupBy(_.suit).toList.map(_._2.size)
+      cardsInEachSuit.forall(_ == cardsInEachSuit.head) should be (true)
+    }
+
+    it("should only have four different suits") {
+      cards.groupBy(_.suit) should have size (4)
+    }
+
+    it("should have a mean value of 8") {
+      cards.map(_.value).sum / cards.size.toDouble should be (8.0)
+    }
+
+    it("should have sum of all cards equal to (2 to 14).sum * 4") {
+      cards.map(_.value).sum should be ((2 to 14).sum * 4)
+    }
+  }
+
 
 }
