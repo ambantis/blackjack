cc5078c1d854ee7e351c33a3138487b4a36aa828
Alexandros Bantis <ambantis@gmail.com>
2014-01-30 20:29:18 -0800

    begin working on actor system

---
+++ src/main/scala/com/ambantis/Boot.scala
@@ -0,0 +1,17 @@
+package com.ambantis
+
+import akka.actor.{Props, ActorSystem}
+
+/**
+ * Boot
+ * User: Alexandros Bantis
+ * Date: 1/30/14
+ * Time: 7:54 PM
+ */
+class Boot extends App {
+
+  implicit val system = ActorSystem("BlackJack")
+
+
+
+}
